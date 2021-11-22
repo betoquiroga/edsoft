db803238071c227d36beead58119ef5d4c1c07d5
Beto Quiroga <beto@ed.team>
2021-11-22 02:42:34 -0400

    Corrige el mensaje del banner

--- components/Home/Banner.jsx
+++ components/Home/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
     <section className="main-banner section-container">
       <div className="content-wrapper content">
         <div className="container">
-          <h1 className="title">Bienvenidos a EDsoft</h1>
+          <h1 className="title">Bienvenidos a EDsoft - 2021</h1>
           <p className="subtitle">Necesitas acesoramiento urgente!!! Nosotros te podemos ayudar en la construcción de tu sitio web.</p>
           <div>
             <a className="button white" href="/contacto.html">Escríbenos ya mismo!</a>
